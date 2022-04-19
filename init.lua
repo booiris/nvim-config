02ce052214cef4b2a80d241b93e57cf22abedbc4
booiris <954520770@qq.com>
2022-04-19 11:06:34 +0800

    update

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 if not vim.g.vscode then
+    vim.cmd("set mouse=a")
     vim.cmd("let g:copilot_filetypes = {\'cpp\': v:false,}")
     vim.cmd("autocmd TextYankPost * if v:event.operator is \'y\' && v:event.regname is \'\' | OSCYankReg \" | endif")
     vim.cmd("set clipboard& clipboard^=unnamed,unnamedplus")

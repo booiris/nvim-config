7baa7f17cf37d0541221aa35c8460b8879688265
booiris <954520770@qq.com>
2022-04-20 00:01:24 +0800

    update

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -6,7 +6,7 @@ return require('packer').startup(function(use)
     -- highlight
     use 'itchyny/vim-cursorword'
 
-    -- scroll 
+    -- scroll
     use 'karb94/neoscroll.nvim'
 
     -- starup time optimise
@@ -15,7 +15,7 @@ return require('packer').startup(function(use)
     use 'nathom/filetype.nvim'
 
     -- buffer
-    use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+    use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons' }
     use 'moll/vim-bbye' -- for more sensible delete buffer cmd
 
     -- themes (disabled other themes to optimize startup time)
@@ -27,7 +27,7 @@ return require('packer').startup(function(use)
     --    use 'ahmedabdulrahman/aylin.vim'
 
     -- file tree
-    use {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons'}
+    use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
 
     -- language
     use 'neovim/nvim-lspconfig'
@@ -41,13 +41,13 @@ return require('packer').startup(function(use)
     use 'p00f/nvim-ts-rainbow'
     use 'onsails/lspkind-nvim'
     use 'github/copilot.vim'
-    use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+    use { 'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp' }
     use 'windwp/nvim-autopairs'
     -- git
     use 'airblade/vim-gitgutter' -- TODO: better git integration
 
     -- status line
-    use {'nvim-lualine/lualine.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+    use { 'nvim-lualine/lualine.nvim', requires = 'kyazdani42/nvim-web-devicons' }
 
     -- tagbar
     use 'simrat39/symbols-outline.nvim'
@@ -56,19 +56,19 @@ return require('packer').startup(function(use)
     use 'voldikss/vim-floaterm'
 
     -- file telescope
-    use {'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim'}
+    use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
 
     -- startup screen
     --    use '~/Developer/aleph-nvim'
     use 'p-z-l/aleph-nvim'
-    
-    -- center 
+
+    -- center
     use 'smithbm2316/centerpad.nvim'
 
     -- vim-blank line
     use 'lukas-reineke/indent-blankline.nvim'
 
-    -- code runner 
+    -- code runner
     use 'CRAG666/code_runner.nvim'
 
     -- show code error tourble
@@ -77,16 +77,15 @@ return require('packer').startup(function(use)
     --    requires = "kyazdani42/nvim-web-devicons",
     --    config = function()
     --    require("trouble").setup {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
     --   }
     -- clipboard
     use 'ojroques/vim-oscyank'
-    
-    -- show hot key 
+
+    -- show hot key
     use 'folke/which-key.nvim'
 
-  end
+end
 )
-
